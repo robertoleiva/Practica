bc6b9f09c90654d0ef820ac08d4cb5c463480720
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2021-02-24 18:53:18 -0600

    Add files via upload

---
+++ practica.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+    
+    
+    $mes="";
+	$numdia;
+	$dia="";
+
+	$dias="";
+
+ 	if(isset($_POST['btnMostrar'])){
+
+	$estado=true;
+
+ 	if($meses = "meses"  && $numdia = "Numero de dias"){
+
+	switch($meses){
+
+	case 1:
+		$mes = "Enero";
+		    break;		
+
+	case 2:
+		$mes = "Febrero";
+		    break;
+	case 3:
+		$mes = "Marzo";
+		    break;		
+		
+
+
+      
+
+	}
+	switch($numdia && $mes){
+	case 1:
+        $numdia = "Enero";
+	case 2:
+        $numdia = "Enero";
+	case 3:
+        $numdia = "Enero";
+	case 4:
+        $numdia = "Enero";
+	case 5:
+        $numdia = "Enero";
+	case 6:
+        $numdia = "Enero";
+	case 7:
+        $numdia = "Enero";
+	case 8:
+        $numdia = "Enero";
+	case 9:
+        $numdia = "Enero";
+	case 10:
+        $numdia = "Enero";
+	case 11:
+        $numdia = "Enero";
+	case 12:
+        $numdia = "Enero";
+	case 13:
+        $numdia = "Enero";
+	case 14:
+        $numdia = "Enero";
+	case 15:
+        $numdia = "Enero";
+	case 16:
+        $numdia = "Enero";
+	case 17:
+        $numdia = "Enero";
+	case 18:
+        $numdia = "Enero";
+	case 19:
+        $numdia = "Enero";
+	case 20:
+        $numdia = "Enero";
+	case 21:
+        $numdia = "Enero";
+	case 22:
+        $numdia = "Enero";
+	case 23:
+        $numdia = "Enero";
+	case 24:
+        $numdia = "Enero";
+	case 25:
+        $numdia = "Enero";
+	case 26:
+        $numdia = "Enero";
+	case 27:
+        $numdia = "Enero";
+	case 28:
+        $numdia = "Enero";
+	case 29:
+        $numdia = "Enero";
+	case 30:
+        $numdia = "Enero";
+	case 31:
+        $numdia = "Enero";
+	$mes1 = "Enero";
+		break;
+        
+
+        switch($dias){
+            case
+            1:
+              $dia = "Lun";
+              break;
+    
+            case
+            2:
+              $dia = "Mar";
+              break;
+            
+            case
+            3:
+              $dia = "Mie";
+              break;
+    
+            case
+            4:
+              $dia = "Jue";
+              break;
+    
+            case
+            5:
+              $dia = "Vie";
+              break;
+    
+            case
+            6:
+              $dia = "Sab";
+              break;
+    
+            case
+            7:
+              $dia = "Dom";
+            
+            
+            $mes = "Enero";
+              break;
+    
+    
+           }
+
+
+	case 1:
+        $numdia = "Febrero";
+	case 2:
+        $numdia = "Febrero";
+	case 3:
+        $numdia = "Febrero";
+	case 4:
+        $numdia = "Febrero";
+	case 5:
+        $numdia = "Febrero";
+	case 6:
+        $numdia = "Febrero";
+	case 7:
+        $numdia = "Febrero";
+	case 8:
+        $numdia = "Febrero";
+	case 9:
+        $numdia = "Febrero";
+	case 10:
+        $numdia = "Febrero";
+	case 11:
+        $numdia = "Febrero";
+	case 12:
+        $numdia = "Febrero";
+	case 13:
+        $numdia = "Febrero";
+	case 14:
+        $numdia = "Febrero";
+	case 15:
+        $numdia = "Febrero";
+	case 16:
+        $numdia = "Febrero";
+	case 17:
+        $numdia = "Febrero";
+	case 18:
+        $numdia = "Febrero";
+	case 19:
+        $numdia = "Febrero";
+	case 20:
+        $numdia = "Febrero";
+	case 21:
+        $numdia = "Febrero";
+	case 22:
+        $numdia = "Febrero";
+	case 23:
+        $numdia = "Febrero";
+	case 24:
+        $numdia = "Febrero";
+	case 25:
+        $numdia = "Febrero";
+	case 26:
+        $numdia = "Febrero";
+	case 27:
+        $numdia = "Febrero";
+	case 28:
+        $numdia = "Febrero";
+	$mes2 = "Febrero";
+		break;
+
+
+
+	case 1: 
+        $numdia = "Marzo";
+
+	case 2:
+        $numdia = "Marzo";
+	case 3:
+        $numdia = "Marzo";
+	case 4:
+        $numdia = "Marzo";
+	case 5:
+        $numdia = "Marzo";
+	case 6:
+        $numdia = "Marzo";
+	case 7:
+        $numdia = "Marzo";
+	case 8:
+        $numdia = "Marzo";
+	case 9:
+        $numdia = "Marzo";
+	case 10:
+        $numdia = "Marzo";
+	case 11:
+        $numdia = "Marzo";
+	case 12:
+        $numdia = "Marzo";
+	case 13:
+        $numdia = "Marzo";
+	case 14:
+        $numdia = "Marzo";
+	case 15:
+        $numdia = "Marzo";
+	case 16:
+        $numdia = "Marzo";
+	case 17:
+        $numdia = "Marzo";
+	case 18:
+        $numdia = "Marzo";
+	case 19:
+        $numdia = "Marzo";
+	case 20:
+        $numdia = "Marzo";
+	case 21:
+        $numdia = "Marzo";
+	case 22:
+        $numdia = "Marzo";
+	case 23:
+        $numdia = "Marzo";
+	case 24:
+        $numdia = "Marzo";
+	case 25:
+        $numdia = "Marzo";
+	case 26:
+        $numdia = "Marzo";
+	case 27:
+        $numdia = "Marzo";
+	case 28:
+        $numdia = "Marzo";
+	case 29:
+        $numdia = "Marzo";
+	case 30:
+        $numdia = "Marzo";
+	case 31:
+        $numdia = "Marzo";
+	$mes3 = "Marzo";
+		break;
+
+
+
+	}
+
+
+
+	
+
+	switch($dias){
+
+        case
+		1:
+		  $dia = "Lun";
+		  break;
+
+        case
+		2:
+		  $dia = "Mar";
+		  break;
+		
+        case
+		3:
+		  $dia = "Mie";
+		  break;
+
+        case
+		4:
+		  $dia = "Jue";
+		  break;
+
+        case
+		5:
+		  $dia = "Vie";
+		  break;
+
+        case
+		6:
+		  $dia = "Sab";
+		  break;
+        case
+		7:
+		  $dia = "Dom";
+		
+		
+		$mes = "Febrero";
+		  break;
+
+
+	   }
+
+	switch($dias){
+        case
+		1:
+		  $dia = "Lun";
+		  break;
+
+        case
+		2:
+		  $dia = "Mar";
+		  break;
+		
+        case
+		3:
+		  $dia = "Mie";
+		  break;
+
+        case
+		4:
+		  $dia = "Jue";
+		  break;
+
+        case
+		5:
+		  $dia = "Vie";
+		  break;
+
+        case
+		6:
+		  $dia = "Sab";
+		  break;
+
+        case
+		7:
+		  $dia = "Dom";
+		
+		
+		$mes = "Marzo";
+		  break;
+
+
+	   } 
+
+     }
+
+   }
+
+
+?>
+
+
+<form action="" method="POST">
+    <table border="1" width="800" align="center" cellpadding="12px" cellspacing="0px">
+
+
+    <tr>
+    <td>Meses</td>
+    <td align="center"><select name="Mes" required>
+    <option  value = "1">Enero</option>
+    <option  value = "2">Febreo</option>
+    <option  value = "3">Marzo</option>
+    <option  value = "4">Abril</option>
+    <option  value = "5">Mayo</option>
+    <option  value = "6">Junio</option>
+    <option  value = "7">Julio</option>
+    <option  value = "8">Agosto</option>
+    <option  value = "9">Septiembre</option>
+    <option  value = "10">Octubre</option>
+    <option  value = "11">Noviembre</option>
+    <option  value = "12">Diciembre</option>
+    </select></td>
+    </tr>
+    <tr>
+    
+    <tr>
+    <td colspan="2"  align="center"> <input type="submit" value="mostrar" name="btnMostrar"> </td>
+            
+    </tr>
+        
+    
+</table>
+</form>
+  
+</body>
+</html>
